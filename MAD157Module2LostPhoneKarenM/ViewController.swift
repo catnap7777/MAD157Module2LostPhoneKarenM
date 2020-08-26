bbff2b4f717e94ae7dcc49939c3a71a16d9cbad8
km <35552602+catnap7777@users.noreply.github.com>
2020-08-26 10:06:44 -0500

    initial commit

--- MAD157Module2LostPhoneKarenM/ViewController.swift
+++ MAD157Module2LostPhoneKarenM/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var buttonObject: UIButton!
+    @IBOutlet var displayText: UITextView!
+    @IBOutlet var thankYouImage: UIImageView!
+    @IBOutlet var instructionLabel: UILabel!
+    
+    var displayOwnerText = "Owner: Karen Mathes \nEmail: kmathes348@students.mchenry.edu \nAlt.Phone Contact: 999-999-9999 \nContact code: \"Hexadecimal\" \nReward for safe return: $25"
+    
+    var thankYouMsg = "thankYou.png"
+    var youAreAwesomeMsg = "You are awesome!!!"
+     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func touchInside(_ sender: UIButton) {
+    
+        
+        buttonObject.setTitle("Please Contact", for: UIControl.State.normal)
+        buttonObject.backgroundColor = UIColor.purple
+        buttonObject.setTitleColor(UIColor.white, for: UIControl.State.normal)
+        
+        displayText.text = self.displayOwnerText
+        instructionLabel.text = self.youAreAwesomeMsg
+        instructionLabel.textColor = UIColor.magenta
+        instructionLabel.font = UIFont.boldSystemFont(ofSize: 24)
+        
+        //thankYouImage = UIImageView(image: UIImage(named: "myImage"))
+        
+        thankYouImage.image = UIImage(named: thankYouMsg)
+    
+         print("****** this is what you have in the text box when button is clicked: \(displayOwnerText)")
+        
+    }
+    
 }
 
