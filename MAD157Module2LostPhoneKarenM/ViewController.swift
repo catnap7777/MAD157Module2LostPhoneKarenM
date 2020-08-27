31fb18f5f9efe222ba30597a8be0696c1132b0b9
km <35552602+catnap7777@users.noreply.github.com>
2020-08-27 14:06:02 -0500

    final

--- MAD157Module2LostPhoneKarenM/ViewController.swift
+++ MAD157Module2LostPhoneKarenM/ViewController.swift
@@ -26,24 +26,24 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func touchInside(_ sender: UIButton) {
-
-        buttonObject.setTitle("Please Contact", for: UIControl.State.normal)
-        buttonObject.backgroundColor = UIColor.purple
-        buttonObject.setTitleColor(UIColor.white, for: UIControl.State.normal)
-        
-        displayText.text = self.displayOwnerText
-        instructionLabel.text = self.youAreAwesomeMsg
-        instructionLabel.textColor = UIColor.magenta
-        instructionLabel.font = UIFont.boldSystemFont(ofSize: 24)
-        
-        thankYouImage.image = UIImage(named: thankYouMsg)
-        
-        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "confetti.png")!)
+    @IBAction func touchUpInside(_ sender: UIButton) {
     
-         print("****** this is what you have in the text box when button is clicked: \(displayOwnerText)")
+        buttonObject.setTitle("Please Contact", for: UIControl.State.normal)
+            buttonObject.backgroundColor = UIColor.purple
+            buttonObject.setTitleColor(UIColor.white, for: UIControl.State.normal)
+            
+            displayText.text = self.displayOwnerText
+            instructionLabel.text = self.youAreAwesomeMsg
+            instructionLabel.textColor = UIColor.magenta
+            instructionLabel.font = UIFont.boldSystemFont(ofSize: 24)
+            
+            thankYouImage.image = UIImage(named: thankYouMsg)
+            
+            self.view.backgroundColor = UIColor(patternImage: UIImage(named: "confetti.png")!)
+        
+             print("****** this is what you have in the text box when button is clicked: \(displayOwnerText)")
+            
+        }
         
     }
     
-}
-
